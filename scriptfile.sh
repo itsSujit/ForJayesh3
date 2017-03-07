320011365484d30dacf8a8472021333e7f57dcca
itsSujit <sujit.karande111@gmail.com>
2017-03-07 10:10:59 +0530

    Update scriptfile.sh

--- scriptfile.sh
+++ scriptfile.sh
@@ -3,15 +3,3 @@ mkdir /home/adzadmin/test/${BUILD_NUMBER}
 mv /home/adzadmin/test/gatewayserv*.zip /home/adzadmin/test/${BUILD_NUMBER}/
 cd /home/adzadmin/test/${BUILD_NUMBER}
 unzip gatewayserv-*.zip
-if [ $HOSTNAME == "ip-172-30-2-88.ec2.internal" ] 
-then 
-    mv /home/adzadmin/test/${BUILD_NUMBER}/dev-application.properties /home/adzadmin/test/${BUILD_NUMBER}/application.properties
-elif [ $HOSTNAME == "ip-172-30-2-206" ] 
-then 
-    mv /home/adzadmin/test/${BUILD_NUMBER}/test-application.properties /home/adzadmin/test/${BUILD_NUMBER}/application.properties
-elif [ $HOSTNAME== "ip-172-30-2-82.ec2.internal" ]
-then 
-    mv /home/adzadmin/test/${BUILD_NUMBER}/demo-application.properties /home/adzadmin/test/${BUILD_NUMBER}/application.properties
-else
-    echo "You are on wrong server"
-fi
