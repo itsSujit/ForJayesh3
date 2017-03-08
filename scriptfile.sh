da9aaf3a8eeab9ad34c7c38b4cb64e5cc986b964
itsSujit <sujit.karande111@gmail.com>
2017-03-08 16:36:11 +0530

    Update scriptfile.sh

--- scriptfile.sh
+++ scriptfile.sh
@@ -3,4 +3,9 @@ mkdir /home/adzadmin/test/${BUILD_NUMBER}
 mv /home/adzadmin/test/gatewayserv*.zip /home/adzadmin/test/${BUILD_NUMBER}/
 cd /home/adzadmin/test/${BUILD_NUMBER}
 unzip gatewayserv-*.zip
-mkdir /home/adzadmin/test/${dev_env}
+if [ $HOSTNAME = ${dev_env} ] 
+then 
+    mv /home/adzadmin/test/${BUILD_NUMBER}/dev-application.properties /home/adzadmin/test/${BUILD_NUMBER}/application.properties
+else
+    echo "You are on wrong server"
+fi
