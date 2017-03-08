7745dc14c38199632d263c8d7fcd93b4cf99e61b
itsSujit <sujit.karande111@gmail.com>
2017-03-08 16:33:59 +0530

    Update scriptfile.sh

--- scriptfile.sh
+++ scriptfile.sh
@@ -3,4 +3,4 @@ mkdir /home/adzadmin/test/${BUILD_NUMBER}
 mv /home/adzadmin/test/gatewayserv*.zip /home/adzadmin/test/${BUILD_NUMBER}/
 cd /home/adzadmin/test/${BUILD_NUMBER}
 unzip gatewayserv-*.zip
-mkdir /home/adzadmin/test/$dev_env
+mkdir /home/adzadmin/test/${dev_env}
