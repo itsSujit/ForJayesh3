33a4a7be84c58e2ff1ab80a104b11da93b5a19e4
itsSujit <sujit.karande111@gmail.com>
2017-03-08 15:12:57 +0530

    Update scriptfile.sh

--- scriptfile.sh
+++ scriptfile.sh
@@ -3,10 +3,4 @@ mkdir /home/adzadmin/test/${BUILD_NUMBER}
 mv /home/adzadmin/test/gatewayserv*.zip /home/adzadmin/test/${BUILD_NUMBER}/
 cd /home/adzadmin/test/${BUILD_NUMBER}
 unzip gatewayserv-*.zip
-echo $dev_env
-if [ $HOSTNAME = $dev_env ] 
-then 
-    mv /home/adzadmin/test/${BUILD_NUMBER}/dev-application.properties /home/adzadmin/test/${BUILD_NUMBER}/application.properties
-else
-    echo "You are on wrong server"
-fi
+echo "serevr hostname is $dev_env"
