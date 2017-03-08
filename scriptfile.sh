781ca504c02a095a650a5873bdfd58fedd4e80bc
itsSujit <sujit.karande111@gmail.com>
2017-03-08 15:33:35 +0530

    Update scriptfile.sh

--- scriptfile.sh
+++ scriptfile.sh
@@ -3,4 +3,4 @@ mkdir /home/adzadmin/test/${BUILD_NUMBER}
 mv /home/adzadmin/test/gatewayserv*.zip /home/adzadmin/test/${BUILD_NUMBER}/
 cd /home/adzadmin/test/${BUILD_NUMBER}
 unzip gatewayserv-*.zip
-echo "serevr hostname is $dev_env"
+mkdir /home/adzadmin/test/$dev_env"
