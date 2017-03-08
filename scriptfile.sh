c2b0ee80194561c4c2439ff84527e95b9fda008e
itsSujit <sujit.karande111@gmail.com>
2017-03-08 15:08:17 +0530

    Update scriptfile.sh

--- scriptfile.sh
+++ scriptfile.sh
@@ -3,8 +3,8 @@ mkdir /home/adzadmin/test/${BUILD_NUMBER}
 mv /home/adzadmin/test/gatewayserv*.zip /home/adzadmin/test/${BUILD_NUMBER}/
 cd /home/adzadmin/test/${BUILD_NUMBER}
 unzip gatewayserv-*.zip
-echo $dev
-if [ $HOSTNAME = $dev ] 
+echo $dev_env
+if [ $HOSTNAME = $dev_env ] 
 then 
     mv /home/adzadmin/test/${BUILD_NUMBER}/dev-application.properties /home/adzadmin/test/${BUILD_NUMBER}/application.properties
 else
