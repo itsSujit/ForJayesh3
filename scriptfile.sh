1ad9c38a5da840f833ef45441b004638fed42fee
itsSujit <sujit.karande111@gmail.com>
2017-03-09 15:30:04 +0530

    Update scriptfile.sh

--- scriptfile.sh
+++ scriptfile.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-mkdir /home/adzadmin/test/${BUILD_NUMBER}
-mv /home/adzadmin/test/gatewayserv*.zip /home/adzadmin/test/${BUILD_NUMBER}/
-cd /home/adzadmin/test/${BUILD_NUMBER}
+mkdir /home/adzadmin/test/${BUILD_ID}
+mv /home/adzadmin/test/gatewayserv*.zip /home/adzadmin/test/${BUILD_ID}/
+cd /home/adzadmin/test/${BUILD_ID}
 unzip gatewayserv-*.zip
 if [ $HOSTNAME = ${dev_env} ] 
 then 
-    mv /home/adzadmin/test/${BUILD_NUMBER}/dev-application.properties /home/adzadmin/test/${BUILD_NUMBER}/application.properties
+    mv /home/adzadmin/test/${BUILD_ID}/dev-application.properties /home/adzadmin/test/${BUILD_ID}/application.properties
 elif [ $HOSTNAME == ${test_env} ] 
 then 
-    mv /home/adzadmin/test/${BUILD_NUMBER}/test-application.properties /home/adzadmin/test/${BUILD_NUMBER}/application.properties
+    mv /home/adzadmin/test/${BUILD_ID}/test-application.properties /home/adzadmin/test/${BUILD_ID}/application.properties
 else
     echo "You are on wrong server"
 fi
